9e6e256c1c69e23b1198a206380c6ac6b471e4e0
jarbasal <jarbasai@mailfence.com>
2020-10-13 20:55:35 +0100

    examples cleanup

--- examples/inspirobot.py
+++ examples/inspirobot.py
@@ -7,12 +7,9 @@ class Inspirobot(FirefoxBrowser):
 
     def generate(self):
         self.driver.get(self.homepage)
-        xpath = "/html/body/div[2]/div[1]/div[1]/div[2]/div"
-        button = self.wait_for_xpath(xpath)
-        button.click()
-
+        self.find_and_click_xpath("/html/body/div[2]/div[1]/div[1]/div[2]/div")
         picture = self.wait_for_css_selector(".generated-image")
-        return picture.get_attribute("src")
+        return self.get_element_attribute(picture, "src")
 
 
 if __name__ == "__main__":
@@ -23,12 +20,13 @@ if __name__ == "__main__":
 
     # Using context manager
     with Inspirobot(geckodriver) as bot:
-       url = bot.generate()
+        url = bot.generate()
     """
 BrowserEvents.BROWSER_OPEN {'open_tabs': ['15'], 'tab_id': '15', 'homepage': 'https://inspirobot.me/'}
-BrowserEvents.WAIT_FOR_XPATH {'xpath': '/html/body/div[2]/div[1]/div[1]/div[2]/div', 'timeout': 30, 'tab_id': '15', 'url': 'https://inspirobot.me/'}
-BrowserEvents.XPATH_FOUND {'xpath': '/html/body/div[2]/div[1]/div[1]/div[2]/div', 'timeout': 30, 'tab_id': '15', 'url': 'https://inspirobot.me/', 'element_text': 'Generate', 'href': None, 'element_id': '7070cd65-9a4d-4634-960d-f9776aa7b296'}
+BrowserEvents.WAIT_FOR_XPATH {'xpath': '/html/body/div[2]/div[1]/div[1]/div[2]/div', 'timeout': 10, 'tab_id': '15', 'url': 'https://inspirobot.me/'}
+BrowserEvents.XPATH_FOUND {'xpath': '/html/body/div[2]/div[1]/div[1]/div[2]/div', 'timeout': 10, 'tab_id': '15', 'url': 'https://inspirobot.me/', 'element_text': 'Generate', 'href': None, 'element_id': '909e70af-535f-450c-b858-92726076ce62'}
+BrowserEvents.ELEMENT_CLICKED {'xpath': '/html/body/div[2]/div[1]/div[1]/div[2]/div', 'element_text': 'Generate', 'tab_id': '15', 'url': 'https://inspirobot.me/', 'href': None}
 BrowserEvents.WAIT_FOR_CSS {'css_selector': '.generated-image', 'timeout': 30, 'tab_id': '15', 'url': 'https://inspirobot.me/'}
-BrowserEvents.CSS_FOUND {'css_selector': '.generated-image', 'timeout': 30, 'tab_id': '15', 'url': 'https://inspirobot.me/', 'element_text': '', 'href': 'https://generated.inspirobot.me/a/rVJqw1KQ6w.jpg', 'element_id': 'b31c8dfc-4710-4e6e-a265-735d65cb0aab'}
+BrowserEvents.CSS_FOUND {'css_selector': '.generated-image', 'timeout': 30, 'tab_id': '15', 'url': 'https://inspirobot.me/', 'element_text': '', 'href': 'https://generated.inspirobot.me/a/DdP0mzKg67.jpg', 'element_id': '8304217a-d004-43d5-86b9-7b28fcf19a6b'}
 BrowserEvents.BROWSER_CLOSED {'open_tabs': ['15'], 'tab_id': '15', 'current_url': 'https://inspirobot.me/', 'tab2url': {'15': 'https://inspirobot.me/'}}
     """
