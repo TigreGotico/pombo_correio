459443d442e080956824583ba6001eea6015ce19
jarbasal <jarbasai@mailfence.com>
2020-10-13 01:30:45 +0100

    tor support

--- examples/tor.py
+++ examples/tor.py
@@ -2,14 +2,17 @@ from pombo_correio import TorBrowser
 
 if __name__ == "__main__":
     from os.path import join, dirname, expanduser
-    from time import sleep
 
-    exec_path = join(dirname(__file__), "geckodriver")
+    gecko_driver = join(dirname(__file__), "geckodriver")
     # https://github.com/mozilla/geckodriver/releases
 
     binary = expanduser("~/Downloads/tor-browser_en-US/Browser/firefox")
     # Using context manager
-    with TorBrowser(exec_path, headless=True, images_enabled=False) as browser:
+    with TorBrowser(gecko_driver,
+                    headless=True,
+                    images_enabled=False,
+                    js_enabled=False,
+                    tor_ff_binary=binary) as browser:
         url = browser.find_and_click_xpath("/html/body/div[2]/p[2]/a[2]")
         browser.wait_for_xpath("/html/body/div/div[4]/div[2]/h2/span")
         browser.save_screenshot("tor_relay.png")
